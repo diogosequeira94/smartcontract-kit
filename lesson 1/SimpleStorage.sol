f9c2035515e2fccdaf1d2803766cce93f4acbd77
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-09-19 12:46:45 +0100

    added structs

--- lesson 1/SimpleStorage.sol
+++ lesson 1/SimpleStorage.sol
@@ -4,6 +4,13 @@ contract SimpleStorage {
     // this will get initialized to 0!
     uint256 favoriteNumber;
 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
@@ -11,5 +18,9 @@ contract SimpleStorage {
     // view, pure
     function retrive() public view returns (uint256) {
         return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
     }
 }
